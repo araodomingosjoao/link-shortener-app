ff61349aab5abc159242d7fd48c0eff4fb3a04b0
Arão Domingos <araodomingos@gmail.com>
2023-08-23 17:56:45 +0100

    Dinamizando os indicadores

--- src/components/Statistic.vue
+++ src/components/Statistic.vue
@@ -1,40 +1,49 @@
+<script setup>
+import { reactive, onMounted } from "vue";
+import axios from "../helpers/axios";
+
+const statistics = reactive({
+  links: [],
+  views: []
+});
+
+const getStatistic = async () => {
+  Promise.all([
+    await axios.get("/links").then((response) => {
+      console.log(response);
+      statistics.links = Object.assign(statistics.links, response?.data?.data);
+    }),
+    await axios.get("/access").then((response) => {
+      console.log(response);
+      statistics.views = Object.assign(statistics.views, response?.data?.data);
+    }),
+  ])
+};
+
+onMounted(async () => {
+  await getStatistic()
+});
+</script>
+
 <template>
   <section class="statistic statistic2">
     <div class="container">
       <div class="row">
-        <div class="col-md-6 col-lg-3">
+        <div class="col-md-6 col-lg-6">
           <div class="statistic__item statistic__item--green">
-            <h2 class="number">10,368</h2>
-            <span class="desc">members online</span>
+            <h2 class="number">{{ statistics.links.length }}</h2>
+            <span class="desc">Links</span>
             <div class="icon">
-              <i class="zmdi zmdi-account-o"></i>
+              <i class="zmdi zmdi-link"></i>
             </div>
           </div>
         </div>
-        <div class="col-md-6 col-lg-3">
+        <div class="col-md-6 col-lg-6">
           <div class="statistic__item statistic__item--orange">
-            <h2 class="number">388,688</h2>
-            <span class="desc">items sold</span>
+            <h2 class="number">{{ statistics.views.length }}</h2>
+            <span class="desc">Clicks</span>
             <div class="icon">
-              <i class="zmdi zmdi-shopping-cart"></i>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-3">
-          <div class="statistic__item statistic__item--blue">
-            <h2 class="number">1,086</h2>
-            <span class="desc">this week</span>
-            <div class="icon">
-              <i class="zmdi zmdi-calendar-note"></i>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-3">
-          <div class="statistic__item statistic__item--red">
-            <h2 class="number">$1,060,386</h2>
-            <span class="desc">total earnings</span>
-            <div class="icon">
-              <i class="zmdi zmdi-money"></i>
+              <i class="zmdi zmdi-eye"></i>
             </div>
           </div>
         </div>
